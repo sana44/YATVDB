5a1961a71a986d16accf1b009055f0c91b42ac69
administrateur <sana.bouichkharen@hotmail.fr>
2017-05-30 13:08:21 +0200

    add crud to season and route to showSeason with her episodes

--- src/SerieBundle/Controller/DefaultController.php
+++ src/SerieBundle/Controller/DefaultController.php
@@ -34,15 +34,13 @@ class DefaultController extends Controller
     public function serieListAction($name)
     {
         $em = $this->getDoctrine()->getManager();
+        
         $category = $em->getRepository('SerieBundle:SerieCategory')->findOneBy(['name' => $name]); 
         if (!$category) {
             throw $this->createNotFoundException('Pas de série existante dans cette catégorie');
         }
-        //$deleteForm = $this->createDeleteForm($id);
         return $this->render('SerieBundle:Default:serieList.html.twig', array(
-            //'serie'      => $serie,
             'category'  => $category,
-            //'delete_form' => $deleteForm->createView(),
 
         ));
     }
